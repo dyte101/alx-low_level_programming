e3adbbc468b815f7e241cc8065525ac7ef5b021d
dyte101 <abubakarr.turay@yahoo.com>
2022-06-23 13:57:21 +0000

    Using puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - The main entry point of the program
+*
+* Description:This program prints 'Programming is like building
+* a multilingual puzzle' using puts
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
