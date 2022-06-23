f01cf5058d652e2cf086ea8da91629b4b8454746
dyte101 <abubakarr.turay@yahoo.com>
2022-06-23 14:33:34 +0000

    Fixes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - The main entry point of the program
+*
+* Description: A program that prints exactly
+* "with proper grammar, but the outcome is a
+* piece of art,"
+*
+* Return:  0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
