298cdd66731298e44954b39ff5f21d48432dfc75
dyte101 <abubakarr.turay@yahoo.com>
2022-06-24 08:20:55 +0000

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,12 @@
 * Description: A program that prints exactly "and that piece
 * of art is useful" - Dora Korpar, 2015-10-19"
 *
-* Return: 0
+* Return: 1
 */
 
 int main(void)
 {
 	char sentence_arr [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, sentence_arr, sizeof(sentence_arr));
-	return 0;
+	return 1;
 }
