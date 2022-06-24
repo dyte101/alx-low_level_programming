0309e8c04d144908ddcd05e26a5c6d0cee6c5cc9
dyte101 <abubakarr.turay@yahoo.com>
2022-06-24 08:43:51 +0000

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,6 @@
 */
 int main(void)
 {
-	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	/* */
-	write(2, s, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
