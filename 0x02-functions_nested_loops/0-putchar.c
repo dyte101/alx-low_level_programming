202dbb03582c3f84e8f46c6dfb19c6aad0d04864
dyte101 <abubakarr.turay@yahoo.com>
2022-07-05 09:45:04 +0000

    Mandatory tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include main.h
 /**
 * main - The main entry point of the program
 *
@@ -9,7 +9,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	char word[10] = "_putchar";
+	int i;
 
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
