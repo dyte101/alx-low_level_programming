0bb690190e6fac138692311475dd9ae478e52d25
dyte101 <abubakarr.turay@yahoo.com>
2022-07-05 11:13:06 +0000

    Mandatory tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * print_sign - main entry
 *
@@ -13,14 +12,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar("+");
 		return (1);
 	} else if (n == 0)
 	{
-		printf("0");
+		_putchar("0");
 		return (0);
 	}
 
-	printf("-");
+	_putchar("-");
 	return (-1);
 }
