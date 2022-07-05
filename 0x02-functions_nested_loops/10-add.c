83dd945a5b77495c692c28a542b33d5b332ced3f
dyte101 <abubakarr.turay@yahoo.com>
2022-07-05 18:28:18 +0000

    Vis Commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - main entry point
+ * 
+ * Description: adds two numbers
+ * 
+ * @num1 - first number
+ * @num2 - second number
+ * 
+ * Return: 0
+ */
+void add(int num1, int num2)
+{
+    return (num1 + num2);
+}
