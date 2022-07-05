17e556ea2875df6d7ad1ce1bc7e9fba10b7aa4cb
dyte101 <abubakarr.turay@yahoo.com>
2022-07-05 18:48:47 +0000

    VS

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * add - main entry point
- * 
+ *
  * Description: adds two numbers
- * 
- * @num1 - first number
- * @num2 - second number
- * 
+ *
+ * @num1 : first number
+ * @num2 : second number
+ *
  * Return: 0
  */
 int add(int num1, int num2)
