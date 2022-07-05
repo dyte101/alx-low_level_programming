471fca34fd63ebd370f60a6504a8166d569cb8b9
dyte101 <abubakarr.turay@yahoo.com>
2022-07-05 09:41:09 +0000

    Mandatory tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - The main entry point of the program
+*
+* Description: Prints _putchar followed by a new line
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
