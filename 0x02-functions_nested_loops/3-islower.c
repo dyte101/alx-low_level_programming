a2f2765263c4e6d06782e68c44229caec909bd69
dyte101 <abubakarr.turay@yahoo.com>
2022-07-05 10:55:49 +0000

    Mandatory tasks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _islower - main entry point
+*
+* Description: checks for lower case
+*
+* @c : check character
+* Return: 1
+*/
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
