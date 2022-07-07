f9e0850c6ac55fc9c0c1acb1861e2f16e1646699
dyte101 <abubakarr.turay@yahoo.com>
2022-07-07 10:32:19 +0000

    Man tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: Test positive_or_negative function
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
