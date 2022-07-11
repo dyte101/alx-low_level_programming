fc8f09b3b485aa4920984ba570057f112d889041
dyte101 <abubakarr.turay@yahoo.com>
2022-07-11 19:03:20 +0000

    Man tasks

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int length, index;
+	char temp;
+
+	length = 0;
+
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+	length--;
+	for (index = 0; index < length; index++)
+	{
+		temp = *(s + length);
+		*(s + length) = *(s + index);
+		*(s + index) = temp;
+		length--;
+	}
+	
+}
