6011514ae7ccb4805929511233ef1a7838a39b77
dyte101 <abubakarr.turay@yahoo.com>
2022-07-14 17:49:48 +0000

    Fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,8 @@ void reverse_array(int *a, int n)
 {
 	int mid_point = n / 2, index = 0, decr_index = index - 1;
 
-	for (mid_point; mid_point > 0; mid_point--, index++)
+	for (; index < 0; mid_point, index++)
 	{
-		a[n - decr_index] += a[index];
-		a[index] = a[n - decr_index] - a[index];
-		a[n - decr_index] = a[n - decr_index] - a[index];
+		a[index] = a[n - decr_index];
 	}
 }
