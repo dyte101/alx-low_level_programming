0a0620fe98527893d7c88d11eb0eeb761a7078e5
dyte101 <abubakarr.turay@yahoo.com>
2022-07-14 17:32:34 +0000

    Fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int src_index = -1, dest_index = 0;
+	int dest_index = 0, src_index = 0;
 
-	for (; dest[dest_index] != '\0'; dest_index++)
-	;
+	while (dest[dest_index] != 0)
+	{
+		dest_index++;
+	}
 
-	do {
-		src_index++;
+	while (src[src_index] != 0)
+	{
 		dest[dest_index] = src[src_index];
-	} while (src[src_index] != '\0');
+		dest_index++;
+		src_index++;
+	}
 
 	return (dest);
 }
