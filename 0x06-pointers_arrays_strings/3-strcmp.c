d988bd414e0a0d03beac5a674fdc046540e21f54
dyte101 <abubakarr.turay@yahoo.com>
2022-07-14 17:37:33 +0000

    Fixes

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,10 @@ char *_strcmp(char *s1, char *s2)
 {
 	int index = 0;
 
-	for (; s1[index] != '\0' || s2[index] != '\0'; index++)
+	while (s1[index] == s2[index] && s1[index] != '\0')
 	{
-		if (s1[index] != s2[index])
-		{
-			return (s1[index] - s2[index]);
-		}
+		index++;
 	}
 
-	return (0);
+	return (s1[index] - s2[index]);
 }
