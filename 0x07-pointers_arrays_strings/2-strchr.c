ad83d204fca9b07a3fe28c0d6c2baf82a56fa739
dyte101 <abubakarr.turay@yahoo.com>
2022-07-18 15:17:18 +0000

    Man tasks

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * _*strchr - locates a character in a string
+ * @s: string
+ * @c: character to find
+ * Return a pointer to the first occurence of the character
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	while(s != '\0' && s != c)
+	{
+		s++;
+	}
+	if (s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+
+}
