7fd2bde5e09c97da58e27fcfb1e009b8d32a0051
dyte101 <abubakarr.turay@yahoo.com>
2022-07-18 23:14:59 +0000

    Fix

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+/**
+ * *_strstr - locates a substring
+ * @haystack: string
+ * @needle: pointer
+ * Return: pointer to the beginning of the located substring
+ */
+
+
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+
+for (i = 0; haystack[i] > '\0'; i++)
+{
+	for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+	{
+		if (haystack[j] != needle[j - i])
+		{
+			break;
+		}
+	}
+	if (needle[j - i] == '\0')
+	{
+		return (haystack + i);
+	}
+}
+return ('\0');
+}
