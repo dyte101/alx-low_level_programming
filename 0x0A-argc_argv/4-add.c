7920040baf473b92407fc4adaf5f745cfe0d463d
dyte101 <abubakarr.turay@yahoo.com>
2022-07-22 16:24:17 +0000

    Man tasks

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add positive numbers
+ * @argc: argument counter
+ * @argv: agument array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, result = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		result += atoi(argv[i]);
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
