0ff0e854bcc2c1985238cab7f19d620b05f8acf6
dyte101 <abubakarr.turay@yahoo.com>
2022-07-22 15:04:57 +0000

    Man tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point to program
+ * @argc: argument counter
+ * @argv: agument array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
